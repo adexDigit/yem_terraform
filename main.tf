79e3ca4e064531196fa699766bd5efdc7a975a1c
adexDigit <32796025+adexDigit@users.noreply.github.com>
2024-08-27 22:53:07 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,13 +9,13 @@ variable "AWS_SECRET_ACCESS_KEY" {
 }
 
 provider "aws" {
-    region = "us-west-2"
+    region = "us-east-1"
     access_key = var.AWS_ACCESS_KEY_ID
     secret_key = var.AWS_SECRET_ACCESS_KEY
 }
 
 resource "aws_instance" "csn" {
-    ami = "ami-05134c8ef96964280"
+    ami = "ami-05134c8ef96964280"   
     instance_type = "t2.micro"
     tags = {
         Name = "YEMMY-DEMO-INSTANCE",
